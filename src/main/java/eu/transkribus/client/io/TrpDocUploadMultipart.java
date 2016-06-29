d1bcbb3b027628f11001978b4fa9f1d3246b6334
Schorsch <guenter.hackl@uibk.ac.at>
2016-06-29 14:01:45 +0200

    build mets call with 4th arg

--- src/main/java/eu/transkribus/client/io/TrpDocUploadMultipart.java
+++ src/main/java/eu/transkribus/client/io/TrpDocUploadMultipart.java
@@ -68,8 +68,8 @@ public class TrpDocUploadMultipart extends Observable implements Runnable {
 			Mets mets;
 			logger.info("Creating METS file at: " + metsFilePath);
 			//build a mets that points to all files we need
-			//2nd arg: export page files (add to mets filesec), 3rd arg: export alto files
-			mets = TrpMetsBuilder.buildMets(doc, true, false);
+			//2nd arg: export page files (add to mets filesec), 3rd arg: export alto files, 4th arg: export images
+			mets = TrpMetsBuilder.buildMets(doc, true, false, true);
 			
 			try {
 				metsFile = JaxbUtils.marshalToFile(mets, metsFile, TrpDocMetadata.class);
