553c8b50bd6ab4dd7e134fea05682535ed3e6fd3
Schorsch <guenter.hackl@uibk.ac.at>
2016-07-07 16:06:41 +0200

    extended docx test

--- src/test/java/eu/transkribus/client/io/DocxTest.java
+++ src/test/java/eu/transkribus/client/io/DocxTest.java
@@ -11,6 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.security.auth.login.LoginException;
+import javax.ws.rs.ClientErrorException;
 import javax.xml.bind.JAXBException;
 
 import org.docx4j.openpackaging.exceptions.Docx4JException;
@@ -18,6 +19,7 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import com.itextpdf.text.DocumentException;
 
 import eu.transkribus.client.connection.TrpServerConn;
+import eu.transkribus.client.util.SessionExpiredException;
 import eu.transkribus.core.model.beans.TrpDoc;
 import eu.transkribus.core.model.beans.customtags.CustomTagFactory;
 import eu.transkribus.core.model.builder.ExportUtils;
@@ -26,35 +28,49 @@ import eu.transkribus.core.model.builder.pdf.PdfExporter;
 import eu.transkribus.core.model.builder.rtf.TrpRtfBuilder;
 
 public class DocxTest {
-	public static void main(String[] args) throws LoginException, MalformedURLException, DocumentException, IOException, JAXBException, URISyntaxException, Docx4JException{
+	public static void main(String[] args){
 		
 		//TrpServerBentham - Batch2 - 363 pages
-		TrpServerConn conn = new TrpServerConn(TrpServerConn.SERVER_URIS[0], "philip", "test123");
-		//blackening
-		TrpDoc doc3 = conn.getTrpDoc(1, 43, -1);
-		
-		//Konzilsprotokolle - Test_ Textstyleoutput
-		TrpDoc doc4 = conn.getTrpDoc(940, 2867, -1);
-		
-		//for tag output test
-		TrpDoc doc5 = conn.getTrpDoc(940, 2444, -1);
-		
-		TrpDoc doc6 = conn.getTrpDoc(255, 3088, -1);
-		
-		TrpDoc doc7 = conn.getTrpDoc(211, 555, -1);
+		try {
+			TrpServerConn conn = new TrpServerConn(TrpServerConn.SERVER_URIS[0], "philip", "test123");
+			//blackening
+			TrpDoc doc3 = conn.getTrpDoc(1, 43, -1);
 			
-		@SuppressWarnings("serial")
-		Set<Integer> idxs2 = new HashSet<Integer>() {{
-			  add(1); add(2);
-			}};
+			//Konzilsprotokolle - Test_ Textstyleoutput
+			TrpDoc doc4 = conn.getTrpDoc(940, 2867, -1);
 			
-		conn.close();
-		
-		boolean wordbased = false;
-		
-	
-		ExportUtils.storeCustomTagMapForDoc(doc7, wordbased, idxs2, null);
-		DocxBuilder.writeDocxForDoc(doc7, true, true, true, new File("C:/Users/Administrator/DocxTest.rtf"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), true, false, false, false);
+			//for tag output test
+			TrpDoc doc5 = conn.getTrpDoc(940, 2444, -1);
+			
+			TrpDoc doc6 = conn.getTrpDoc(255, 3088, -1);
+			
+			TrpDoc doc7 = conn.getTrpDoc(211, 555, -1);
+				
+			@SuppressWarnings("serial")
+			Set<Integer> idxs2 = new HashSet<Integer>() {{
+				  add(2); add(3);
+				}};
+				
+			conn.close();
+			
+			boolean wordbased = false;
+			
+
+			ExportUtils.storeCustomTagMapForDoc(doc5, wordbased, idxs2, null);
+			//export with substitute abbreviations and preserve line breaks
+			DocxBuilder.writeDocxForDoc(doc5, true, true, true, new File("C:/Users/Administrator/DocxTest1.docx"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), false, false, false, true, true);
+			//export with substitute abbreviations and not preserve line breaks
+			DocxBuilder.writeDocxForDoc(doc5, true, true, true, new File("C:/Users/Administrator/DocxTest2.docx"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), false, false, false, true, false);
+			//export with expand abbreviations and preserve line breaks
+			DocxBuilder.writeDocxForDoc(doc5, true, true, true, new File("C:/Users/Administrator/DocxTest3.docx"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), false, false, true, false, true);
+			//export with expand abbreviations and not preserve line breaks
+			DocxBuilder.writeDocxForDoc(doc5, true, true, true, new File("C:/Users/Administrator/DocxTest4.docx"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), false, false, true, false, false);
+			//not preserve line breaks
+			DocxBuilder.writeDocxForDoc(doc5, true, true, true, new File("C:/Users/Administrator/DocxTest5.docx"), idxs2, null, CustomTagFactory.getRegisteredTagNames(), false, false, false, false, false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
