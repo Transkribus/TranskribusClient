be8ec65f8dae5fafc0a2015ef8b5cf5a5b8d0f02
jkloe <jkloe@whatsoever.com>
2017-03-03 17:12:32 +0100

    TrpJobsConnection for jobMgmt

--- src/main/java/eu/transkribus/client/connection/TrpJobsConnection.java
+++ src/main/java/eu/transkribus/client/connection/TrpJobsConnection.java
@@ -8,15 +8,26 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import eu.transkribus.client.util.JerseyUtils;
 import eu.transkribus.client.util.SessionExpiredException;
+import eu.transkribus.core.model.beans.DocumentSelectionDescriptor;
 import eu.transkribus.core.model.beans.job.TrpJobStatus;
 import eu.transkribus.core.rest.RESTConst;
+import eu.transkribus.core.util.CoreUtils;
+import eu.transkribus.core.util.GsonUtil;
 
 public class TrpJobsConnection extends ATrpServerConn {	
+	private static final Logger logger = LoggerFactory.getLogger(TrpJobsConnection.class);
 	
 	public static TrpJobsConnection create(boolean prodServer) throws LoginException {
 		if (prodServer) {
@@ -44,6 +55,56 @@ public class TrpJobsConnection extends ATrpServerConn {
 		postNull(t);
 	}
 	
+	public List<TrpJobStatus> getPendingJobs(List<String> tasks) throws SessionExpiredException, ServerErrorException, ClientErrorException {
+		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.GET_PENDING_JOBS_PATH);
+		
+		t = queryParam(t, RESTConst.TASKS_PARAM, CoreUtils.join(tasks));
+		
+		return getList(t, JOB_LIST_TYPE);
+	}
+	
+	public boolean scheduleJob(String jobId) throws SessionExpiredException, ServerErrorException, ClientErrorException {
+		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.SCHEDULE_JOB_PATH);
+		
+		t = queryParam(t, RESTConst.JOB_ID_PARAM, jobId);
+		
+		Response resp = t.request().post(null);
+		
+		checkStatus(resp, t);
+		
+		if (resp.getStatus() != 200) {
+			logger.debug("scheduleJob, status = "+resp.getStatus()+ " - could not schedule job!");
+			logger.debug(""+resp.getEntity());
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public void updateJob(TrpJobStatus job) throws SessionExpiredException, ServerErrorException, ClientErrorException {
+		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.UPDATE_JOB_PATH);
+		t = t.queryParam(RESTConst.JOB_PARAM, job);
+		
+		postEntity(t, job, MediaType.APPLICATION_XML_TYPE);
+	}
+	
+	public TrpJobStatus queryJob(String jobId) throws SessionExpiredException, ServerErrorException, ClientErrorException {
+		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.QUERY_JOB_PATH);
+		t = t.queryParam(RESTConst.JOB_ID_PARAM, jobId);
+		
+		return postEntityReturnObject(t, null, MediaType.APPLICATION_XML_TYPE, TrpJobStatus.class, MediaType.APPLICATION_XML_TYPE);
+	}
+	
+	public List<String> getModuleVersions(String name, boolean releaseOnly) throws SessionExpiredException, ServerErrorException, ClientErrorException {
+		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.GET_MODULE_VERSIONS_PATH);
+		
+		t = t.queryParam(RESTConst.NAME_PARAM, name);
+		t = t.queryParam(RESTConst.RELEASE_ONLY_PARAM, releaseOnly);
+		
+		String json = getObject(t, String.class);
+		return GsonUtil.toStrList2(json);		
+	}
+	
 //	public List<TrpJobStatus> retrieveJobs(String jobType, String jobTask, String toolProvider, String toolVersion, String host) throws SessionExpiredException, ServerErrorException, IllegalArgumentException, ClientErrorException {
 //		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.RETRIEVE_JOBS_PATH);
 //
@@ -56,13 +117,13 @@ public class TrpJobsConnection extends ATrpServerConn {
 //		return getList(t, JOB_LIST_TYPE);
 //	}
 	
-	public List<TrpJobStatus> scheduleJobs(Set<Integer> jobIds) throws SessionExpiredException, ServerErrorException, IllegalArgumentException, ClientErrorException {
-		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.SCHEDULE_JOBS_PATH);
-
-		t = JerseyUtils.queryParam(t, RESTConst.JOB_IDS_PARAM, jobIds);
-
-		return getList(t, JOB_LIST_TYPE);
-	}
+//	public List<TrpJobStatus> scheduleJobs(Set<Integer> jobIds) throws SessionExpiredException, ServerErrorException, IllegalArgumentException, ClientErrorException {
+//		WebTarget t = baseTarget.path(RESTConst.JOB_MGMT_PATH).path(RESTConst.SCHEDULE_JOBS_PATH);
+//
+//		t = JerseyUtils.queryParam(t, RESTConst.JOB_IDS_PARAM, jobIds);
+//
+//		return getList(t, JOB_LIST_TYPE);
+//	}
 	
 	
 
